2201d81bc34a5c9b93599c019ede442b81d9c867
dany <dhad2301@usherbrooke.ca>
2022-08-14 14:10:43 -0400

    Improved game menu look

--- src/Scenes/SceneGameMenu_UI.ts
+++ src/Scenes/SceneGameMenu_UI.ts
@@ -1,18 +1,19 @@
 import { CST } from "../CST";
 import { CYBR_Scene } from "./CYBR_Scene";
+import { CYBR_TextButton } from "../UI/CYBR_TextButton";
 import { CYBR_Button } from "../UI/CYBR_Button";
 import { SceneGame } from "./SceneGame";
 import { CYBR_Graphics } from "../Utils/CYBR_Graphics";
 
 export class SceneGameMenu_UI extends CYBR_Scene
 {
-    private width: number = 500;
-    private height: number = 350;
+    private width: number = 1000;
+    private height: number = 700;
 
     private sceneGame: SceneGame;
-    private buttonResume: CYBR_Button;
-    private buttonRestart: CYBR_Button;
-    private buttonMainMenu: CYBR_Button;
+    private buttonResume: CYBR_TextButton;
+    private buttonRestart: CYBR_TextButton;
+    private buttonMainMenu: CYBR_TextButton;
 
     constructor()
     {
@@ -38,18 +39,20 @@ export class SceneGameMenu_UI extends CYBR_Scene
         let background = new CYBR_Graphics(this);
         background.width = this.width;
         background.height = this.height;
-        background.fillStyle(parseInt(CST.STYLE.COLOR.CYBER_BACKGROUND));
+        background.fillStyle(parseInt(CST.STYLE.COLOR.CYBER_BACKGROUND), 0.75);
         background.fillRect(0, 0, background.width, background.height);
         this.centerItem(background);
 
         // Buttons
-        this.buttonResume = new CYBR_Button(this, 0, 0, "Resume");
+        const buttonStyle = { fontSize : "36px", color: CST.STYLE.COLOR.ORANGE, strokeThickness : 4};
+
+        this.buttonResume =  new CYBR_TextButton(this, 0, 0, "Resume", buttonStyle);
         this.buttonResume.onClicked(this.onResumeClicked, this);
 
-        this.buttonRestart = new CYBR_Button(this, 0, 0, "Restart");
+        this.buttonRestart =  new CYBR_TextButton(this, 0, 0, "Restart", buttonStyle);
         this.buttonRestart.onClicked(this.onRestartClicked, this);
 
-        this.buttonMainMenu = new CYBR_Button(this, 0, 0, "Main menu");
+        this.buttonMainMenu =  new CYBR_TextButton(this, 0, 0, "Main menu", buttonStyle);
         this.buttonMainMenu.onClicked(this.onMainMenuClicked, this);
 
         this.updateButtonsAlignment();
@@ -66,7 +69,7 @@ export class SceneGameMenu_UI extends CYBR_Scene
                 visibleButtons.push(button);
         }
 
-        const spacing = 24;
+        const spacing = 36;
         const totalHeight = visibleButtons[0].height * visibleButtons.length + spacing * (visibleButtons.length - 1);
         this.centerItem(visibleButtons[0], 0, (visibleButtons[0].height -totalHeight) * 0.5);
 
