1f26d4a5e32738974caed8bc6c0f289bc5c8613a
dany <dhad2301@usherbrooke.ca>
2022-09-17 09:05:17 -0400

    Hide settings page until shortcuts issue has been fixed

--- src/UI/MainMenu/MainMenuContainer.ts
+++ src/UI/MainMenu/MainMenuContainer.ts
@@ -37,9 +37,9 @@ export class MainMenuContainer extends Phaser.GameObjects.Container
         selectLevelTextButton.onClicked(this.onSelectLevelClicked, this);
         this.add(selectLevelTextButton);
 
-        const settingsIconButton = new CYBR_IconButton(scene, 930, 28, "UI_atlas", "iconGear.png");
-        settingsIconButton.onClicked(this.onSettingsClicked, this);
-        this.add(settingsIconButton);
+        // const settingsIconButton = new CYBR_IconButton(scene, 930, 28, "UI_atlas", "iconGear.png");
+        // settingsIconButton.onClicked(this.onSettingsClicked, this);
+        // this.add(settingsIconButton);
 
         // const discordTextButton = new CYBR_TextButton(scene, 800, 12, "Discord", { fontSize : "20px", color: CST.STYLE.COLOR.ORANGE, strokeThickness : 2});
         // discordTextButton.onClicked(this.onDiscordConnectClicked, this);
