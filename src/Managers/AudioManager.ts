59af024baba0e4d42baa2c302bc7d611ebe1071b
dany <dhad2301@usherbrooke.ca>
2022-09-17 08:37:13 -0400

    Audio: Temporary disable sounds

--- src/Managers/AudioManager.ts
+++ src/Managers/AudioManager.ts
@@ -8,25 +8,31 @@ export class AudioManager {
     public static init(audiosprite: any)
     {
         this.howl = new Howl(audiosprite);
-        AudioManager.unmute();
+        AudioManager.mute();
     }
 
     public static mute(): void
     {
         if (!!AudioManager.howl)
+        {
             AudioManager.howl.mute(true);
+        }
     }
 
     public static unmute(): void
     {
-        if (!!AudioManager.howl)
-            AudioManager.howl.mute(false);
+        // if (!!AudioManager.howl)
+        // {
+        //     AudioManager.howl.mute(false);
+        // }
     }
 
     public static playSound(key: string, loop: boolean = false, volume: number = 1): number
     {
         if (loop && AudioManager.loopKeys.includes(key)) // This sound is already playing in loop
+        {
             return AudioManager.loopIDs[AudioManager.loopKeys.indexOf(key)];
+        }
 
         const id = AudioManager.howl.play(key);
         AudioManager.howl.volume(volume, id);
